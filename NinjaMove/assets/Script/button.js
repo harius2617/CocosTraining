949a2e9bbb9e675efa9e2536b9a3689dc6e5619e
harius2617 <danghai2617@gmail.com>
2021-12-12 23:55:20 +0700

    update

--- NinjaMove/assets/Script/button.js
+++ NinjaMove/assets/Script/button.js
@@ -1,18 +1,59 @@
 const Emitter = require('mEmitter');
 cc.Class({
     extends: cc.Component,
+    properties: {
+        btnRight: cc.Button,
+        btnLeft: cc.Button,
+        btnJump: cc.Button,
+        btnReset: cc.Button
+    },
     start () {
     },
     moveRight(){
-    Emitter.instance.emit('RIGHT');
+        Emitter.instance.emit('RIGHT');
+        this.btnLeft.interactable = false;
+        this.btnJump.interactable = false;
+        this.btnReset.interactable = false;
+        cc.tween(this.node)
+            .delay(1)
+            .call(()=> {
+                this.btnLeft.interactable = true;
+                this.btnReset.interactable = true;
+                this.btnJump.interactable = true;
+            })
+            .start()
     },
     moveLeft(){
-    Emitter.instance.emit('LEFT');
+        Emitter.instance.emit('LEFT');
+        this.btnRight.interactable = false;
+        this.btnJump.interactable = false;
+        this.btnReset.interactable = false;
+        cc.tween(this.node)
+            .delay(1)
+            .call(()=> {
+                this.btnRight.interactable = true;
+                this.btnReset.interactable = true;
+                this.btnJump.interactable = true;
+            })
+            .start()
     },
     jump() {
         Emitter.instance.emit("JUMP");
+        this.btnJump.interactable = false;
+        this.btnLeft.interactable = false;
+        this.btnRight.interactable = false;
+        this.btnReset.interactable = false;
+        cc.tween(this.node)
+            .delay(2)
+            .call(()=> {
+                this.btnJump.interactable = true;
+                this.btnLeft.interactable = true;
+                this.btnReset.interactable = true;
+                this.btnRight.interactable = true;
+            })
+            .start()
     },
     reset(){
         Emitter.instance.emit("RESET")
     }
-    });
+});
--- NinjaMove/library/imports/4c/4c4a8dc1-fe2c-48f0-bc90-babb00666407.js
+++ NinjaMove/library/imports/4c/4c4a8dc1-fe2c-48f0-bc90-babb00666407.js
@@ -7,15 +7,53 @@ cc._RF.push(module, '4c4a83B/ixI8LyQursAZmQH', 'button');
 var Emitter = require('mEmitter');
 cc.Class({
     extends: cc.Component,
+    properties: {
+        btnRight: cc.Button,
+        btnLeft: cc.Button,
+        btnJump: cc.Button,
+        btnReset: cc.Button
+    },
     start: function start() {},
     moveRight: function moveRight() {
+        var _this = this;
+
         Emitter.instance.emit('RIGHT');
+        this.btnLeft.interactable = false;
+        this.btnJump.interactable = false;
+        this.btnReset.interactable = false;
+        cc.tween(this.node).delay(1).call(function () {
+            _this.btnLeft.interactable = true;
+            _this.btnReset.interactable = true;
+            _this.btnJump.interactable = true;
+        }).start();
     },
     moveLeft: function moveLeft() {
+        var _this2 = this;
+
         Emitter.instance.emit('LEFT');
+        this.btnRight.interactable = false;
+        this.btnJump.interactable = false;
+        this.btnReset.interactable = false;
+        cc.tween(this.node).delay(1).call(function () {
+            _this2.btnRight.interactable = true;
+            _this2.btnReset.interactable = true;
+            _this2.btnJump.interactable = true;
+        }).start();
     },
     jump: function jump() {
+        var _this3 = this;
+
         Emitter.instance.emit("JUMP");
+        this.btnJump.interactable = false;
+        this.btnLeft.interactable = false;
+        this.btnRight.interactable = false;
+        this.btnReset.interactable = false;
+        cc.tween(this.node).delay(2).call(function () {
+            _this3.btnJump.interactable = true;
+            _this3.btnLeft.interactable = true;
+            _this3.btnReset.interactable = true;
+            _this3.btnRight.interactable = true;
+        }).start();
     },
     reset: function reset() {
         Emitter.instance.emit("RESET");
