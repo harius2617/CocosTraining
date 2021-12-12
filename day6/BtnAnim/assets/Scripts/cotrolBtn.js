633abf3f69fb45b448c34acbeffd3c19f189dbb4
harius2617 <danghai2617@gmail.com>
2021-12-12 14:12:21 +0700

    update

--- day6/BtnAnim/assets/Scripts/cotrolBtn.js
+++ day6/BtnAnim/assets/Scripts/cotrolBtn.js
@@ -18,198 +18,319 @@ cc.Class({
     },
 
     easeInSineBtn() {
-        let action = cc.moveBy(4, 450, 0);
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4, 450, 0),cc.callFunc(callback));
         action.easing(cc.easeSineIn(6.0));
         this.node.runAction(action);
     },
 
     easeOutSine(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeSineOut(6.0));
         this.node.runAction(action);
     },
 
     easeInOutSine() {
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeSineInOut(6.0));
         this.node.runAction(action);
     },
 
     easeInQuad(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeQuadraticActionIn(3.0));
         this.node.runAction(action);
     },
 
     easeOutQuad() {
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeQuadraticActionOut(3.0));
         this.node.runAction(action);
     },
 
     easeInOutQuad() {
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeQuadraticActionInOut(3.0));
         this.node.runAction(action);
-        
     },
 
     easeInBack() {
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeBackIn(3.0));
         this.node.runAction(action);
     },
 
     easeInCubic(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeCubicActionIn(3.0));
         this.node.runAction(action);
     },
 
     easeOutCubic(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeCubicActionOut(3.0));
         this.node.runAction(action);
     },
 
     easeInOutCubic(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeCubicActionInOut(3.0));
         this.node.runAction(action);
     },
     
     easeInQuart(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeQuarticActionIn(3.0));
         this.node.runAction(action);
     },
 
     easeOutQuart(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeQuarticActionOut(3.0));
         this.node.runAction(action);
     },
 
     easeInOutQuart(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeQuarticActionInOut(3.0));
         this.node.runAction(action);
     },
 
     easeInQuint(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeQuinticActionIn(3.0));
         this.node.runAction(action);
     },
 
     easeOutQuint(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeQuinticActionOut(3.0));
         this.node.runAction(action);
     },
 
     easeInOutQuint(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeQuinticActionInOut(3.0));
         this.node.runAction(action);
     },
 
     easeInExpo(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeExponentialIn(3.0));
         this.node.runAction(action);
     },
 
     easeOutExpo(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeExponentialOut(3.0));
         this.node.runAction(action);
     },
 
     easeInOutExpo(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeExponentialInOut(3.0));
         this.node.runAction(action);
     },
 
     easeInCirc(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeCircleActionIn(3.0));
         this.node.runAction(action);
     },
 
     easeOutCirc(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeCircleActionOut(3.0));
         this.node.runAction(action);
     },
 
     easeInOutCirc(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeCircleActionInOut(3.0));
         this.node.runAction(action);
     },
 
     easeOutBack() {
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeBackOut(3.0));
         this.node.runAction(action);
     },
 
     easeInOutBack() {
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeBackInOut(3.0));
         this.node.runAction(action);
     },
 
     easeInElastic(){
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeElasticIn(3.0));
         this.node.runAction(action);
     },
 
     easeOutElastic() {
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeElasticOut(3.0));
         this.node.runAction(action);
     },
 
     easeInOutElastic() {
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeElasticInOut(3.0));
         this.node.runAction(action);
     },
 
     easeInBounce() {
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequenceq(cc.moveBy(4,450,0),cc.callFunc(callback));
         action.easing(cc.easeBounceIn(3.0));
         this.node.runAction(action);
     },
 
     easeOutBounce() {
-        let action = cc.moveBy(4,450,0)
+        this.spAnim.setAnimation(0, "run", true);
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0), cc.callFunc(callback));
         action.easing(cc.easeBounceOut(3.0));
         this.node.runAction(action);
     },
 
     easeInOutBounce() {
         this.spAnim.setAnimation(0, "run", true);
-        let action = cc.moveBy(4,450,0)
+        const callback = () => {
+            this.spAnim.setAnimation(0, "idle", true)
+        }
+        let action = cc.sequence(cc.moveBy(4,450,0), cc.callFunc(callback))
         action.easing(cc.easeBounceInOut(3.0));
         this.node.runAction(action);
     },
     resetBtn() {
+        this.node.stopAllActions()
         this.node.position = this._firstPos;
-        this.spAnim.setAnimation(0, "aim", false);
+        this.spAnim.setAnimation(0, "idle", false);
     },
     onLoad () {
         this._firstPos = this.node.position;
-        this.spAnim.setAnimation(0, "aim", false);
+        this.spAnim.setAnimation(0, "idle", false);
     },
     start () {
-        this.spAnim.setCompleteListener(() => {
-            this.spAnim.setAnimation(0, "aim", true);
-        })
+        // this.spAnim.setCompleteListener((entry) => {
+        //     if(entry.animation.name === 'run'){
+        //         this.spAnim.setAnimation(0, "aim", true);
+        //     }
+        // })
         this.spAnim.setEventListener((entry, event)=> {
             const {data} = event;
             let audioID = cc.audioEngine.play(this.stepSound, false, 1);
